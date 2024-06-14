fa212b1a0bdaa5e8130b3575ad572ccd06fa9fcf
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2024-06-15 02:48:04 +0900

    카메라 y축 이동 추가

--- Odyssey/Assets/Script/Util/CameraMover.cs
+++ Odyssey/Assets/Script/Util/CameraMover.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
+    GameObject player;
     [SerializeField] float speed = 5f;
+    [SerializeField] float yFollowSpeed = 2f;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>().gameObject;
+    }
 
     private void FixedUpdate()
     {
+        // 이동 속도로 오른쪽으로 이동
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        // 플레이어의 y 위치를 따라 카메라의 y 위치 변경
+        float newYPosition = Mathf.Lerp(transform.position.y, player.transform.position.y, yFollowSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
     }
 }
